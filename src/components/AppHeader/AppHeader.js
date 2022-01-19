771e954018c1cf8c35c57c5272f2be97270c9e0a
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-01-19 21:12:20 +0700

    Finish header

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -2,12 +2,42 @@ import React from "react";
 
 import "./AppHeader.css"
 
+const getTimeAndDate = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const day = date.getDay();
+    const daynumber = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (minutes < 10) {
+        return `${days[day]} ${daynumber} ${months[month]} ${year} ${hours}:0${minutes}`;
+    } else {
+        return `${days[day]} ${daynumber} ${months[month]} ${year} ${hours}:${minutes}`;
+    }
+};
+
 const AppHeader  = () => {
     return (
         <div className="AppHeader">
-            <h1>This is header</h1>
+            <div className="left">
+                <span className="fab fa-apple apple-logo"></span>
+                <span className="menus active">Finder</span>
+                <span className="menus">File</span>
+                <span className="menus">Edit</span>
+                <span className="menus">View</span>
+                <span className="menus">Go</span>
+                <span className="menus">Window</span>
+                <span className="menus">Help</span>
+            </div>
+
+            <div className="right">      
+                <div className="active menu-time">{getTimeAndDate()}
+                </div>
         </div>
+    </div>
     );
 };
-
 export default AppHeader;
