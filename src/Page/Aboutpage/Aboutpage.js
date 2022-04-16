ef34fc92e9cc522d13d5e4c9aae4a7fbedc2f734
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-04-16 21:33:49 +0700

    Fixing responsive

---
+++ src/Page/Aboutpage/Aboutpage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import './Aboutpage.css'
+
+const Aboutpage = () => {
+  return (
+    <div className="AppPage">
+        <div className="HeaderPage">
+          <i className="fas fa-circle dotnav"></i>
+          <i className="fas fa-circle dotnav yellow"></i>
+          <i className="fas fa-circle dotnav green"></i>
+        </div>
+        <div className="ContainerText">
+          
+        </div>
+        <div className="Footer">
+          <Footer />
+        </div>
+      </div>
+  )
+}
+
+export default Aboutpage
