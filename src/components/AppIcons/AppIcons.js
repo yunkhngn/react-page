fea621da50ba7c12f932ac31ce0159b17ce7df1a
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-04-16 23:12:28 +0700

    Refixing redirect

--- src/components/AppIcons/AppIcons.js
+++ src/components/AppIcons/AppIcons.js
@@ -17,7 +17,7 @@ const AppIcons = ({ icon, name, theme, showDot, setShowDot, clickHandler, url })
     <div className="IconContainer">
       {false && <span className="AppIcon--label">{name}</span>}
       <div
-        onClick={() => setShowDot(true)}
+        onClick={() => {setShowDot()}}
         className={bounce ? "bounce icon2" : "icon2"}
       >
         <span
