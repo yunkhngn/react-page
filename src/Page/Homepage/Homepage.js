c44110dee379b30c4a5880ef7c24946699f7c7c6
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-04-16 21:48:58 +0700

    Under constructions

--- src/Page/Homepage/Homepage.js
+++ src/Page/Homepage/Homepage.js
@@ -11,7 +11,8 @@ const Homepage = () => {
           <i className="fas fa-circle dotnav green"></i>
         </div>
         <div className="ContainerText">
-          
+          <h1>Sites are under constructions</h1>
+          <a href="https://yunkhngn.github.io/portfolio/">Please click this for portfolio</a>
         </div>
         <div className="Footer">
           <Footer />
