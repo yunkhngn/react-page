fa2fad52eaccf42541e264b2cb3c81253157dcab
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-04-23 01:47:22 +0700

    Meta tags

--- src/components/MetaTag/MetaTag.js
+++ src/components/MetaTag/MetaTag.js
@@ -5,10 +5,9 @@ const MetaTag = (titlePage) => {
     return (
     <MetaTags>
         <title>{titlePage.titlePage + " | Khoa Nguyễn"}</title>
-        
         <meta id="meta-description" name="description" content="Khoa Nguyen is a software engineer in Hanoi, Vietnam. He enjoys building software with Reactjs and dealing with algorithms and data structures." />
         <meta id="og-title" property="og:title" content={titlePage.titlePage + " | Khoa Nguyễn"} />
-        <meta id="og-image" property="og:image" content="https://www.khoanguyen.dev/static/media/wall.32ce37cd.png" />
+        <meta id="og-image" property="og:image" content="/static/media/wall.32ce37cd.png" />
         <meta property="og:image:alt" content="A rainy day"/>
         <meta property="og:url" content={"https://www.khoanguyen.dev/" + titlePage.titlePage.toLowerCase().replace(/\s/g, '')}/>
         <meta property="og:type" content="website"/>
@@ -17,7 +16,7 @@ const MetaTag = (titlePage) => {
         <meta property="twitter:url" content={"https://www.khoanguyen.dev/" + titlePage.titlePage.toLowerCase().replace(/\s/g, '')}/>
         <meta property="twitter:title" content={titlePage.titlePage + " | Khoa Nguyễn"}/>
         <meta property="twitter:description" content="Khoa Nguyen is a software engineer in Hanoi, Vietnam. He enjoys building software with Reactjs and dealing with algorithms and data structures."/>
-        <meta property="twitter:image" content="https://www.khoanguyen.dev/static/media/wall.32ce37cd.png"/>
+        <meta property="twitter:image" content="/static/media/wall.32ce37cd.png"/>
     </MetaTags>
   )
 }
