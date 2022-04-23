956f0bc077a2960197bf4ab62e0830ec66f086ba
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-04-24 03:07:50 +0700

    Error example

--- src/Page/Playground/Playground.js
+++ src/Page/Playground/Playground.js
@@ -1,17 +1,35 @@
 import React from 'react'
-import {Div} from 'atomize'
-import Notfound from '../Notfound/Notfound'
+import {Button, Div, scrollTo} from 'atomize'
+// import Notfound from '../Notfound/Notfound'
 
 const Playground = () => {
-  if (process.env.NODE_ENV === 'production') {
+  // if (process.env.NODE_ENV === 'production') {
     
-    return <Notfound/> 
-  }
+  //   return <Notfound/> 
+  // }
+  // else{
   return (
     <Div>
-     
+      <Div h="100vh" bg="blue100" d="flex" align="center" justify="center">
+        <Button
+            id="blueButton"
+            onClick={() => scrollTo("#yellowButton")}
+            bg="info700"
+          >
+            Scroll To Yellow Button
+        </Button>
+      </Div>
+      <Div h="100vh" bg="blue100" d="flex" align="center" justify="center">
+        <Button
+            id="yellowButton"
+            onClick={() => scrollTo("#blueButton", 100, 0, 800)}
+            bg="warning700"
+          >
+            Scroll To Blue Button
+        </Button>
+      </Div>
     </Div>
   )
 }
-
+// }
 export default Playground
--- src/components/AnimatedPage/AnimatedPage.js
+++ src/components/AnimatedPage/AnimatedPage.js
@@ -1,6 +1,7 @@
 import React, {lazy} from 'react'
 import {useLocation, Routes, Route} from "react-router-dom";
 import {AnimatePresence} from 'framer-motion/dist/framer-motion'
+
 const Homepage = lazy(() => import("../../Page/Homepage/Homepage"));
 const Aboutpage = lazy(() => import("../../Page/Aboutpage/Aboutpage"));
 const Picturepage = lazy(() => import("../../Page/Picturepage/Picturepage"));
@@ -12,16 +13,15 @@ function AnimatedPage() {
   const location = useLocation();
   return (
     <AnimatePresence exitBeforeEnter>
-    <Routes location={location} key={location.pathname}>
-          <Route exact path='*' element={<Notfound />} status={404}/>
-          <Route path="/" element={<Homepage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
-          <Route path="/about"   element={<Aboutpage/>} />
-          <Route path="/pictures" element={<Picturepage/>} />
-          <Route path="/projects" element={<Projectpage/>} />
-          <Route path="/playground" element={<Playground/>} />
-    </Routes>
-    
+        <Routes location={location} key={location.pathname}>
+              <Route exact path='*' element={<Notfound />} status={404}/>
+              <Route path="/" element={<Homepage/>}/>
+              <Route path="/home" element={<Homepage/>}/>
+              <Route path="/about"   element={<Aboutpage/>} />
+              <Route path="/pictures" element={<Picturepage/>} />
+              <Route path="/projects" element={<Projectpage/>} />
+              <Route path="/playground" element={<Playground/>} />
+        </Routes>
     </AnimatePresence>
   )
 }
