fdfc784bc0bca1af866644f2a37f2d1bb0870b4c
Khoa Nguyen <rogkhoa.mail@gmail.com>
2022-04-26 02:24:12 +0700

    Updating pages

--- src/Page/Playground/Playground.js
+++ src/Page/Playground/Playground.js
@@ -1,35 +1,18 @@
 import React from 'react'
-import {Button, Div, scrollTo} from 'atomize'
-// import Notfound from '../Notfound/Notfound'
+import {Div} from 'atomize'
+import Notfound from '../Notfound/Notfound'
 
 const Playground = () => {
-  // if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     
-  //   return <Notfound/> 
-  // }
-  // else{
-  return (
-    <Div>
-      <Div h="100vh" bg="blue100" d="flex" align="center" justify="center">
-        <Button
-            id="blueButton"
-            onClick={() => scrollTo("#yellowButton")}
-            bg="info700"
-          >
-            Scroll To Yellow Button
-        </Button>
+    return <Notfound/> 
+  }
+  else{
+    return (
+      <Div>
+        Lol who found this?
       </Div>
-      <Div h="100vh" bg="blue100" d="flex" align="center" justify="center">
-        <Button
-            id="yellowButton"
-            onClick={() => scrollTo("#blueButton", 100, 0, 800)}
-            bg="warning700"
-          >
-            Scroll To Blue Button
-        </Button>
-      </Div>
-    </Div>
-  )
+    )
+  }
 }
-// }
 export default Playground
